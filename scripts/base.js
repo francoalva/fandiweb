620d12604139c847ded67a2bd1a3a240b46361a0
Franco Alvarez <70734792+francoalva@users.noreply.github.com>
2020-11-28 23:46:01 -0300

    Update base.js

--- scripts/base.js
+++ scripts/base.js
@@ -2,6 +2,6 @@ function changeColor(color){
 	document.body.style.backgroundColor = color;
 }
 
-//var tiempo = setInterval(function(){document.body.style.backgroundColor = "red"}, 1000);
-//var tiempo = setInterval(function(){document.body.style.backgroundColor = "blue"}, 2000);
-//var tiempo = setInterval(function(){document.body.style.backgroundColor = "black"}, 3000);
+var tiempo = setInterval(function(){document.body.style.backgroundColor = "red"}, 1000);
+var tiempo = setInterval(function(){document.body.style.backgroundColor = "blue"}, 2000);
+var tiempo = setInterval(function(){document.body.style.backgroundColor = "black"}, 3000);
